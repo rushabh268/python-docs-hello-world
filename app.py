3789206eb8c5e401460f7b98f2d726f18fc2491e
Rushabh Sanghvi <rushabh.sanghvi@illumio.com>
2021-08-04 17:57:44 -0700

    Updating the app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import json
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    url = 'https://revealer-test-1.azurewebsites.net'
+    r = requests.get(url = url)
+    data = r.json()
+    print data
     return "Hello, World!"
